27bd84cf12f7695eed8c8a5a64ff4fd0f5a101ea
Jose Pagan <jm.pagan@gmail.com>
2019-12-12 20:14:51 +0000

    First Commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const quotes = [
+  {
+    quote:"There is no fear for one whose mind is not filled with desires.",
+    source:"Buddha",
+    // citation:"",
+    year:"c. 563 BCE–c. 483 BCE",
+  },
+  {
+    quote:"Work out your own salvation. Do not depend on others.",
+    source:"Buddha",
+    // citation:"",
+    year:"c. 563 BCE–c. 483 BCE",
+  },
+  {
+    quote:"if anything is worth doing, do it with all your heart.",
+    source:"Buddha",
+    // citation:"",
+    year:"c. 563 BCE–c. 483 BCE",
+  },
+  {
+    quote:"All that we are is the result of what we have thought.",
+    source:"Buddha",
+    // citation:"",
+    year:"c. 563 BCE–c. 483 BCE",
+  },
+  {
+    quote:"Attachment leads to suffering.",
+    source:"Buddha",
+    // citation:"",
+    year:"c. 563 BCE–c. 483 BCE",
+  },
+  {
+    quote:"If your compassion does not include yourself, it is incomplete.",
+    source:"Buddha",
+    // citation:"",
+    year:"c. 563 BCE–c. 483 BCE",
+  },
+  {
+    quote:"Nothing ever exists entirely alone; everything is in relation to everything else.",
+    source:"Buddha",
+    // citation:"",
+    year:"c. 563 BCE–c. 483 BCE",
+  },
+  {
+    quote:"Purity or impurity depends on oneself. No one can purify another.",
+    source:"Buddha",
+    // citation:"",
+    year:"c. 563 BCE–c. 483 BCE",
+  },
+  {
+    quote:"One moment can change a day, one day can change a life and one life can change the world.",
+    source:"Buddha",
+    // citation:"",
+    year:"c. 563 BCE–c. 483 BCE",
+  },
+]
